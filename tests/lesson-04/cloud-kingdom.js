4590e963ca28f08b227739eb1302b13fb19e94f1
test 01 - thamdh <thamk55tha@gmail.com>
2026-06-02 17:36:56 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,13 @@
+let powerUp = 'none';
+let effect = 'Unknown power-up';
+
+if (powerUp === 'mushroom') {
+    effect = 'Mario becomes Super!';
+} else if (powerUp === 'flower') {
+    effect = 'Mario can shoot fireballs!';
+} else if (powerUp === 'star') {
+    effect = 'Mario is invincible!';
+} else if (powerUp ==='none'){
+    effect = 'Mario is normal';
+}
+console.log(effect)
